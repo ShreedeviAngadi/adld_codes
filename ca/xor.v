17e40f434a61bd0f5ca6b28ce53dc274086f3f17
ShreedeviAngadi <102354321+ShreedeviAngadi@users.noreply.github.com>
2022-05-19 09:16:20 +0530

    Add files via upload

---
+++ ca/xor.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:57:36 03/09/2022 
+// Design Name: 
+// Module Name:    xor 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+module xor(a,b,c);
+input a,b;
+output c;
+assign c=a^b;
+end module
