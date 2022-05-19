25752e9c362e6667b9a02e991e7a90d40e6fbed9
ShreedeviAngadi <102354321+ShreedeviAngadi@users.noreply.github.com>
2022-05-19 11:01:04 +0530

    Add files via upload

---
+++ project/rightshift.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:07:08 05/16/2022 
+// Design Name: 
+// Module Name:    rightshift 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module rightshift(m,h);
+input[15:0]h;
+output [15:0] m;
+assign m = h>>1;
+endmodule
+
